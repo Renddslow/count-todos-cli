63ec8fabdf4835b58b1be18c1ec60001954e19aa
Matt McElwee <mmcelwee@dmsi.com>
2019-06-12 12:22:22 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const program = require('commander');
+const countTodos = require('count-todos');
+
+program
+	.version(require('./package').version)
+	.option('-p, --path [path]')
+	.option('-e, --exclude [exclusions]')
+	.option('-f, --fileTypes [fileTypes]')
+	.option('-v, --verbose')
+	.parse(process.argv);
+
+const exclusions = program.exclude ? program.exclude.split(',') : [];
+const fileTypes = program.fileTypes ? program.fileTypes.split(',') : [];
+
+const options = {
+	exclude: exclusions,
+	fileTypes: fileTypes,
+	verbose: program.verbose,
+};
+
+if (program.path) {
+	console.log(countTodos(program.path, options));
+} else {
+	console.log(countTodos(options));
+}
